8ac996226efd6d40ec76e027ef7fc3111ed43dac
cocoswank <patchkatie@gmail.com>
2015-03-16 11:25:03 -0500

    Added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316152343) do
+ActiveRecord::Schema.define(version: 20150316161713) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.string   "image"
+    t.string   "video"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
